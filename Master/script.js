7e65f34f21fb85e26a13bd3135efdfbcbd0531c1
samson wong <steelerwong2005@hotmail.com>
2020-06-10 18:35:58 -0400

    use oneCall API that gives both current and future daily forecast

--- Master/script.js
+++ Master/script.js
@@ -5,9 +5,9 @@
   // call renderHistory - to display histoical searched city
 
 // Listener on search-button
-$("#search-button").on("click", startReport);
+$("#search-button").on("click", fetchData);
    
-// Function startReport:
+// Function fetchData:
   // Extract Value from search-value
   // Save to local storage array. 
   // make API call to weather. 
@@ -19,29 +19,47 @@ var cityDisplay;
 var tempDisplay;
 var humDisplay;
 var windDisplay;
+var uvDisplay;
 
-function startReport (event) {
+
+// Make API call to retrieve current and 5 days objects
+// Then pass to each respective function for further processing
+function fetchData (event) {
    event.stopPropagation();
    event.preventDefault();
 
+   // Retrieve user input city
    city = $("#search-value").val();
+
+   // Fetch current weather data
    wURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+   
    $.ajax({
       url: wURL,
       method: "GET"
-   }).then(wReport)
-} // End startReport
+   }).then(processData)
+} // End fetchData
+
+// This function will use the input obj, extract the Lat and Longtitude info
+// Lat/Lon are required parameters to make oneCall API, which give current and 5 days forecast data
+function processData (wObject) {
+   console.log("Pre processed wObject is: ", wObject);
+
+   //working:  https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&%20&exclude=minutely,hourly&appid=0e3be3e2387201a17d65f7f6c8b863cb
+   oneCallURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + wObject.coord.lat + "&lon=" + wObject.coord.lon + "&exclude=minutely, hourly&appid=" + APIKey;
+   $.ajax({
+      url: oneCallURL,
+      method:"GET"
+   }).then(function(onecallObj) {
+      console.log("Processed object is a one call obj:", onecallObj);
+      renderCurrent(onecallObj);
+      //render5days(x);
+   })
+} // End processData
 
 
-function wReport (wObject) {
-   console.log("wObject is: ", wObject);
-   renderCurrent(wObject);
-   render5days(wObject);
-} // End wReport
-
-
-function renderCurrent(wObject) {
-   console.log(wURL);
+function renderCurrent(weatherObj) {
+   console.log(oneCallURL);
    // Display current weekday, month, and date
    var currentDate = moment().format('dddd') + ", " + moment().format('MMM Do');
    cityDisplay = $("<div>")
@@ -51,32 +69,26 @@ function renderCurrent(wObject) {
          "fontSize":30,
          "padding-bottom": "10px",
       });
-   var fahren = (wObject.main.temp - 273.15) * 1.8 + 32
+   var fahren = (weatherObj.current.temp - 273.15) * 1.8 + 32
    tempDisplay = $("<div>").text("Tempature: " + fahren + "F");
-   humDisplay = $("<div>").text("Humidity: " + wObject.main.humidity + "%");
-   windDisplay = $("<div>").text("Wind Speed: " + wObject.wind.speed + " MPH");
+   humDisplay = $("<div>").text("Humidity: " + weatherObj.current.humidity + "%");
+   windDisplay = $("<div>").text("Wind Speed: " + weatherObj.current.wind_speed + " MPH");
+   uvDisplay = $("<div>").text("UV Index: " + weatherObj.current.uvi);
+
    //working https://api.openweathermap.org/data/2.5/uvi?appid=0e3be3e2387201a17d65f7f6c8b863cb&lat=42.36&lon=-71.06
-   var uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + wObject.coord.lat + "&lon=" + wObject.coord.lon;
-   
-   $.ajax({
-      url: uvURL,
-      method: "GET"
-   }).then(dCurrent)
-
-
+   //var uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + wObject.coord.lat + "&lon=" + wObject.coord.lon;
+   dCurrent();
 } // End renderCurrent
 
-function dCurrent(uvObject) {
-   var uvDisplay = $("<div>").text("UV Level: " + uvObject.value);
-   $("#today").append(cityDisplay, tempDisplay,humDisplay, windDisplay, uvDisplay);
-}
+function dCurrent() {
+   $("#today").empty();
+   $("#today").append(cityDisplay, tempDisplay,humDisplay, windDisplay, uvDisplay).css ({
+      "border-width": "2px",
+      "border-style":"solid",
+      "border-color": "lightgrey"
+   });
+} // End dCurrent
 
-
-
-  // call renderCurrent (response)
-     // today.empty
- // call renderFivedays(response)
-   // forecast.empty
 
 
 
