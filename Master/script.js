60966e1ef99da05ccbb5c51523e99ce3e3242514
samson wong <steelerwong2005@hotmail.com>
2020-06-10 22:26:41 -0400

    current and forecast complete

--- Master/script.js
+++ Master/script.js
@@ -1,16 +1,6 @@
-
-// Call main() funciton
-   // today.empty()
-  // forecase.empty()
-  // call renderHistory - to display histoical searched city
-
 // Listener on search-button
 $("#search-button").on("click", fetchData);
    
-// Function fetchData:
-  // Extract Value from search-value
-  // Save to local storage array. 
-  // make API call to weather. 
 var APIKey = "0e3be3e2387201a17d65f7f6c8b863cb";
 var wURL; 
 var city;
@@ -21,6 +11,7 @@ var humDisplay;
 var windDisplay;
 var uvDisplay;
 var iconDisplay;
+var forecastDay = 1;
 
 
 // Make API call to retrieve current and 5 days objects
@@ -54,17 +45,28 @@ function processData (wObject) {
    }).then(function(onecallObj) {
       console.log("Processed object is a one call obj:", onecallObj);
       renderCurrent(onecallObj);
-      render5days(onecallObj);
+      $("#forecast").empty();
+      $("#forecast").append($("<div>").text("5 day Forecast: ") // Add a title before displaying 5 days forecast
+      .css({
+         "font-weight": "bold",
+         "fontSize":30,
+         "padding-bottom": "10px",
+         "color": "salmon"
+      }));
+
+      // Display 5 days forecast, one by one
+      for (var i = 1; i <=5 ; i++){
+         render5days(onecallObj, i);
+      }
+      
    })
 } // End processData
 
-// Prepare Current Data
+// Prepare Current Weather Data
 function renderCurrent(weatherObj) {
-   console.log(oneCallURL);
-   // Display current weekday, month, and date
-   //var currentDate = moment().format('dddd') + ", " + moment().format('MMM Do');
-   var currentDate = moment().format('l')
 
+   var currentDate = moment().format('l')
+   console.log("Forecast day:", forecastDay, typeof(forecastDay));
    // iconDisplay = $("<img>").attr("src", "http://openweathermap.org/img/wn/10d@2x.png");
    iconDisplay = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + weatherObj.current.weather[0].icon + "@2x.png");
 
@@ -75,13 +77,13 @@ function renderCurrent(weatherObj) {
          "fontSize":30,
          "padding-bottom": "10px",
       }).append(iconDisplay);
-   
 
    tempDisplay = $("<div>").text("Tempature: " + kelvinToF(weatherObj.current.temp) + "F");
    humDisplay = $("<div>").text("Humidity: " + weatherObj.current.humidity + "%");
    windDisplay = $("<div>").text("Wind Speed: " + weatherObj.current.wind_speed + " MPH");
    uvDisplay = $("<div>").text("UV Index: " + weatherObj.current.uvi);
 
+   // Call dCurrent to display Weather data
    dCurrent();
 } // End renderCurrent
 
@@ -97,28 +99,29 @@ function dCurrent() {
 
 
 // Prepare forecast data
-function render5days(forecastObj) {
-   var fDate = $("<div>").text(moment().add(1, 'day').format('l'));
-   var fTemp = $("<div>").text("Temp: " +  kelvinToF(forecastObj.daily[1].temp.max) + "F");
-   var fHum = $("<div>").text("Humidity: " + forecastObj.daily[1].humidity + "%");
-   var fIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + forecastObj.daily[1].weather[0].icon + "@2x.png");
-   dForecast(fDate, fTemp, fHum, fIcon);
+function render5days(forecastObj, fDay) {
+   // .add function add number of day from current date
+   var fDate = $("<div>").text(moment().add(fDay, 'day').format('l'));
+   var fTemp = $("<div>").text("Temp: " +  kelvinToF(forecastObj.daily[fDay].temp.max) + "F");
+   var fHum = $("<div>").text("Humidity: " + forecastObj.daily[fDay].humidity + "%");
+   var fIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + forecastObj.daily[fDay].weather[0].icon + "@2x.png");
+   dForecast(fDate, fTemp, fHum, fIcon, fDay);
 } 
 
 // Display Forecast Data
-function dForecast(fDate, fTemp, fHum, fIcon) {
+function dForecast(fDate, fTemp, fHum, fIcon, fDay) {
    var oneDayContainer = $("<div>").append(fDate, fIcon, fTemp, fHum)
    .css({
       "border-width": "2px",
       "border-style":"solid",
       "border-color": "lightgrey",
       "width": "20%",
-      "background-color": "lightblue"
+      "background-color": "lightblue",
+      "display": "inline-block"       //so each div line up horizontally
    });
-   $("#forecast").append(oneDayContainer);
-   //$("#forecast").append(fDate, fIcon, fTemp, fHum);
 
-}
+   $("#forecast").append(oneDayContainer);
+} // End dForecast
 
 function kelvinToF (kelvin) {
    var fahran = ((kelvin - 273.15) * 1.8 + 32).toFixed(2);
@@ -126,9 +129,5 @@ function kelvinToF (kelvin) {
 }
 
 
-
-
-// Function renderCurrent
-// Function renderFiveDays
 // Function renderHistory
 
